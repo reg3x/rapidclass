aad107137dd071cb4625c86f7f87236d84fc0fee
reg3x <diegoaguilera85@gmail.com>
2015-02-19 16:29:13 -0430

    binding navbar with models

--- lms/views.py
+++ lms/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from models import Teacher, Class, Subject
 
 # Create your views here.
 class Home(TemplateView):
@@ -8,4 +9,6 @@ class Home(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(Home, self).get_context_data()
         context['message']='Welcome to RapidClass 0.1'
+        context['teachers'] = Teacher.objects.all()
+        context['subjects'] = Subject.objects.all()
         return context
