d59fb058fa517f44f388c2a061995ce94f428995
reg3x <diegoaguilera85@gmail.com>
2015-02-19 15:10:25 -0430

    initial view

--- lms/views.py
+++ lms/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+class Home(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(Home, self).get_context_data()
+        context['message']='Welcome to RapidClass 0.1'
+        return context
--- rapidclass/urls.py
+++ rapidclass/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from lms.views import Home
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'rapidclass.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', Home.as_view()),
 
+    # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
