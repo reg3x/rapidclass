68f8536d3957eeb6321e396440408962d4469250
reg3x <diegoaguilera85@gmail.com>
2015-02-26 16:00:10 -0430

    updated router[C

--- rapidclass/urls.py
+++ rapidclass/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from lms.views import Home
+from lms.views import Home, ClassDetail
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', Home.as_view()),
+    url(regex=r'^class/(?P<slug>\w+)/$',
+        view=ClassDetail.as_view(),
+        name='home_view'),
 
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
