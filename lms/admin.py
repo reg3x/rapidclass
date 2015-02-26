4bb5fa4d2e1f010f3eed5bd8867e09b1aaf50f53
reg3x <diegoaguilera85@gmail.com>
2015-02-26 15:58:03 -0430

    deleting middle table from admin

--- lms/admin.py
+++ lms/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from models import Student, Teacher, Class, Subject, InscriptionDetail
+from models import Student, Teacher, Class, Subject, InscriptionDetail, Evaluation, File
+
+
+class PostAdmin(admin.ModelAdmin):
+    exclude = ('slug',)
+
 
 admin.site.register(Student)
 admin.site.register(Teacher)
 admin.site.register(Class)
 admin.site.register(Subject)
 admin.site.register(InscriptionDetail)
+admin.site.register(Evaluation)
+admin.site.register(File)
